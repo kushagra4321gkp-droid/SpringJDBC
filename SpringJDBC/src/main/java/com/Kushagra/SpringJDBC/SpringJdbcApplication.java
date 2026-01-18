ac67cbc5e4d41c371e3e9f9d0e03415a70daeebe
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-18 11:25:46 +0530

    Created A Student Object a One Row in the Table and Every attibute or field of Student will act as a Column in the Table, So we are trying to map our class with Table

--- SpringJDBC/src/main/java/com/Kushagra/SpringJDBC/SpringJdbcApplication.java
+++ SpringJDBC/src/main/java/com/Kushagra/SpringJDBC/SpringJdbcApplication.java
@@ -2,12 +2,21 @@ package com.Kushagra.SpringJDBC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+
+		Student s = context.getBean(Student.class);
+		s.setName("Kushagra");
+		s.setMarks(98);
+		s.setRollNo(30);
+
+		System.out.println(s);
 	}
 
 }
