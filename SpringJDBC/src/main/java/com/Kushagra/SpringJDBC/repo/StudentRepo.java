2397f7024a89b4047504621920ae2e95ba6aaef0
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-19 19:37:23 +0530

    Using RowMapper and Lambda Expression

--- SpringJDBC/src/main/java/com/Kushagra/SpringJDBC/repo/StudentRepo.java
+++ SpringJDBC/src/main/java/com/Kushagra/SpringJDBC/repo/StudentRepo.java
@@ -3,8 +3,11 @@ package com.Kushagra.SpringJDBC.repo;
 import com.Kushagra.SpringJDBC.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,8 +34,22 @@ public class StudentRepo {
 
     public List<Student> findAll(){
 
-        List<Student> student = new ArrayList<>();
-        return student;
+        String sql = "select * from student";
+
+        RowMapper<Student> mapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Student s = new Student();
+                s.setRollNo(rs.getInt("rollNo"));
+                s.setName(rs.getString("name"));
+                s.setMarks(rs.getInt("marks"));
+
+                return s;
+            }
+        };
+
+        return jdbc.query(sql, mapper);
     }
 
 
