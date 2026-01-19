5b6216cfebd3c99c3179e7f7f2e51f0e430c9e3f
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-19 20:10:58 +0530

    Using External DB(mySQL) in SpringJDBC by prividing the config in the application.properties file

--- SpringJDBC/src/main/resources/data.sql
+++ SpringJDBC/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into Student values(101, 'priyam', 81);
 insert into Student values(102, 'aman', 82);
-insert into Student values(103, 'gauri', 78);
+insert into Student values(103, 'gauri', 1000);
